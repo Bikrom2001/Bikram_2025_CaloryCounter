a1550f2bfcb8d60a6f0806d765942b5486c7be53
Bikrom2001 <bikromroy2001@gmail.com>
2026-07-15 20:10:45 +0600

    BasicInfoModel Name, Age, Gender, Height, Weight

--- Bikram_2025_CaloryCounter/CalorieCounter/models.py
+++ Bikram_2025_CaloryCounter/CalorieCounter/models.py
@@ -9,7 +9,25 @@ class User(AbstractUser):
     
     
 
+# Name, Age, Gender, Height, Weight
 
+class BasicInfoModel(models.Model):
+    
+    GENDER_TYPE =[
+        ("Male", "Male"),
+        ("Female", "Female")
+    ]
+    
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='user_info', null=True)
+    name = models.CharField(max_length=100, null=True)
+    age = models.PositiveIntegerField(null=True)
+    gender = models.CharField(choices=GENDER_TYPE, max_length=10, null=True)
+    height = models.FloatField(null=True)
+    weight = models.FloatField(null=True)
+    
+    
+    def __str__(self):
+        return f"{self.name} - {self.age}"
     
     
 
