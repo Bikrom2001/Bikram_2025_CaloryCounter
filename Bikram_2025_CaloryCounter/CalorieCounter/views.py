001e2f6f078637b76454a2e11bbc1dc79093bdb9
Bikrom2001 <bikromroy2001@gmail.com>
2026-07-15 22:44:23 +0600

    Registeration successfully

--- Bikram_2025_CaloryCounter/CalorieCounter/views.py
+++ Bikram_2025_CaloryCounter/CalorieCounter/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages
 from CalorieCounter.models import *
 from CalorieCounter.forms import *
 
 
 def register_page(request):
+    
+    if request.method == "POST":
+        form_data = RegisterationForm(request.POST)
+        if form_data.is_valid():
+            form_data.save()
+            messages.success(request, 'Registeration successfully')
+            return redirect('login_page')
     
     
     form_data = RegisterationForm()
